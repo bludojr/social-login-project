ce3e4e9fa238f99d037f008442c78c71beb9397a
bludojr <ewusidavid8@gmail.com>
2024-10-07 13:39:15 +0000

    Updated code

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const container = document.getElementsByClassName("mail")[0];
 button.addEventListener("click", function (e) {
   container.style.borderColor = "red";
 });
+
+console.log('today')
+
+const firstName = 'David';
+
+const password = new RegExp('David*');
+
+console.log(password.test(firstName));
