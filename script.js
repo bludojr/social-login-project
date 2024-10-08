d632710f1d7b78984446c02fc70ce6fefe314c15
bludojr <ewusidavid8@gmail.com>
2024-10-08 08:11:27 +0000

    Modified Design

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-const emailInput = document.getElementById('mail');
+// const emailInput = document.getElementById('mail');
 const button = document.getElementsByClassName("circle")[0];
 const container = document.getElementsByClassName("mail")[0];
 
 button.addEventListener("click", function (e) {
   container.style.borderColor = "red";
 });
-
-console.log('today')
-
-const firstName = 'David';
-
-const password = new RegExp('David*');
-
-console.log(password.test(firstName));
-
-
-if (password.test(firstName){})
