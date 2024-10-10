79e23ab7a85f729104e762f533767cdf6a677dd6
bludojr <ewusidavid8@gmail.com>
2024-10-10 16:01:45 +0000

    Updated code

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 // const emailInput = document.getElementById('mail');
-const button = document.getElementsByClassName("circle")[0];
-const container = document.getElementsByClassName("mail")[0];
+const button = document.getElementById("button");
+const emailInput = document.getElementById("mail");
+const emailPattern = new RegExp('^ewusidavid8@gmail.com$');
 
-button.addEventListener("click", function (e) {
-  container.style.borderColor = "red";
+
+button.addEventListener("click", (e) =>{
+  e.preventDefault();
+
+  const email = emailInput.value;
+  console.log(emailPattern.test(email));
+
+  
+if (emailPattern.test(email)) {  
+  alert('Valid Email');
+  emailInput.style.borderColor = "green";
+  emailInput.style.border = "5px green solid";
+  
+  
+  setTimeout(()=>{
+    emailInput.style.borderColor = "white";
+    emailInput.style.border = "1px white solid";
+
+  }, 5000 );
+
+ 
+} else {
+  alert('Invalid Email');
+  emailInput.style.borderColor = "red";
+  emailInput.style.border = "5px red solid";
+
+  
+  setTimeout(()=>{
+    emailInput.style.borderColor = "white";
+    emailInput.style.border = "1px white solid";
+
+  }, 5000 );
+
+  
+} ;
 });
+
